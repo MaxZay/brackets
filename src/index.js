99a0b3324b2067f5629f9a239f23767b08d300f9
MaxZay <54762301+MaxZay@users.noreply.github.com>
2021-02-17 09:40:34 +0300

    Решено

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 == 0) {
+        for (let i = str.length - 1; i >= 0; i--) {
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                if (str[i] == bracketsConfig[j][0] || str[i] == bracketsConfig[j][1]) {
+                    let temp = bracketsConfig[j].join('')
+                    str = str.replace(temp, '')
+                }
+            }
+        }
+        if (str.length == 0)
+            return true
+    }
+    return false
 }
